0501af5af6b4a36a871f921f98afbad6e8406125
Leyla Alekperova <leslairy@gmail.com>
2018-08-14 15:26:31 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do
+    'WELCOME'
+  end
+  
+  get '/name' do
+    'Hi, my name is name'
+  end
+  
+  get '/lastname' do
+   'Alek' 
+  end
+  get '/home' do
+    'Doddododdoo'
+  end
+  get '/boring' do 
+    ''
 end
